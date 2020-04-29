4186c24ee2a8e7c042b15b98affc1e23ca214f5c
João Vitor <joto.x42@gmail.com>
2020-04-29 15:03:43 -0300

    Auth Controller

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,13 +1,57 @@
 'use strict'
 
+const Database = require('Database')
+const User = require('App/Models/User')
+const Role = require('Role')
+
 class AuthController {
-    async register({req, res}) {}
+    async register({req, res}) {
+        const trx = await Database.beginTransaction()
+        try {
+            const { name, email, password } = req.all()
+            const user = await User.create({name, email, password}, trx)
+            const userRole = await Role.findBy('slug', 'admin')
+            await user.roles().attach([userRole.id], null, trx)
+            await trx.commit()
+            return res.status(201).send({ data: user })
+        } catch(err) {
+            await trx.rollback()
+            return res.status(400).send({
+                message: 'Else ao realizar cadastro',
+                error: err,
+            })
+        }
+    }
 
-    async login({req, res, auth}) {}
+    async login({req, res, auth}) {
+        const { email, password } = req.all()
+        let data = await auth.withRefreshToken().attempt(email, password)
+        return res.send({ data })
+    }
     
-    async refresh({req, res, auth}) {}
+    async refresh({req, res, auth}) {
+        var refresh_token = req.input('refresh_token')
 
-    async logout({req, res, auth}) {}
+        if(!not) {
+            refresh_token = req.header('refresh_token')
+        }
+
+        const user = await auth.newRefreshToken().generateForRefreshToken(refresh_token)
+
+        return res.send({ data: user })
+    }
+
+    async logout({req, res, auth}) {
+        let refresh_token = request.input('refresh_token')
+
+        if(!refresh_token) {
+            refresh_token = request.header('refresh_token')
+        }
+
+        await auth.authenticator('jwt').revokeTokens([refresh_token], true)
+
+        return res.status(204).send({})
+    }
 }
 
 module.exports = AuthController
