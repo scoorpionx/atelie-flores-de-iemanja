41dc3c15006ed60e380a7933c759c6ac2b27f92e
João Vitor <joto.x42@gmail.com>
2020-04-29 17:22:01 -0300

    User Controller

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -17,7 +17,17 @@ class UserController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ req, res, pagination }) {
+    const name = req.input('name')
+    const query = User.query()
+
+    if(name) {
+      query.where('name', 'LIKE', `%${name}%`)
+      query.orWhere('email', 'LIKE', `%${name}%`)
+    }
+
+    const users = await query.paginate(pagination.page, pagination.limit)
+    return res.send(users)
   }
 
   /**
@@ -40,7 +50,22 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ req, response }) {
+    try {
+      const userData = req.only([
+        'name',
+        'username',
+        'email',
+        'password',
+      ])
+
+      const user = User.create(userData)
+      return res.status(201).send(user)
+    } catch (err) {
+      return res.status(400).send({
+        message: 'Não foi possível criar este usuário no momento!'
+      })
+    }
   }
 
   /**
@@ -52,7 +77,9 @@ class UserController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params: { id }, req, res, view }) {
+    const user = await User.findOrFail(id)
+    return res.send(user)
   }
 
   /**
@@ -75,7 +102,17 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, req, res }) {
+    const user = await User.findOrFail(id)
+    const userData = req.only([
+      'name',
+      'username',
+      'email',
+      'password',
+    ])
+    user.merge(userData)
+    await user.save()
+    return res.send(user)
   }
 
   /**
@@ -86,7 +123,16 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, req, res }) {
+    const user = await User.findOrFail(id)
+    try {
+      await user.delete()
+      return res.status(204).send()
+    } catch (err) {
+      return res.status(500).send({
+        message: 'Não foi possível deletar o usuário no momento'
+      })
+    }
   }
 }
 
