ff1e9915fc01fe47a75a8fd9c0db176b7a299b5d
João Vitor <joto.x42@gmail.com>
2020-04-30 15:38:19 -0300

    Exception Handler

---
+++ app/Exceptions/Handler.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const BaseExceptionHandler = use('BaseExceptionHandler')
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+  /**
+   * Handle exception thrown during the HTTP lifecycle
+   *
+   * @method handle
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   * @param  {Object} options.response
+   *
+   * @return {void}
+   */
+  async handle (error, { req, res }) {
+    if(error.name === 'ValidationException'){
+      res.status(error.status).send({
+        errors: error.messages
+      })
+      return
+    }
+
+    res.status(error.status).send({error: error.message})
+  }
+
+  /**
+   * Report exception for logging or debugging.
+   *
+   * @method report
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   *
+   * @return {void}
+   */
+  async report (error, { request }) {
+  }
+}
+
+module.exports = ExceptionHandler
